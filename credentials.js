cb6cd6cf2eb65a36e125cf0e7f929d67e9a9a3ec
Smakuwa <45312377+Smakuwa@users.noreply.github.com>
2022-04-12 18:22:03 +0200

    eee

---
+++ credentials.js
@@ -0,0 +1,5 @@
+exports.MEMCACHIER_USERNAME = "719D71";
+exports.MEMCACHIER_PASSWORD = "22B8243E274771DAFCA5770E11528C2A";
+exports.MEMCACHIER_SERVERS = "mc1.dev.eu.ec2.memcachier.com";
+exports.FACEBOOK_EMAIL = "smakubot@gmail.com";
+exports.FACEBOOK_PASSWORD = "uczucio1234567890";
