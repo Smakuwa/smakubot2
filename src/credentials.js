b581d41e285764f15241327ab192f3cef3f759e9
Smakuwa <45312377+Smakuwa@users.noreply.github.com>
2022-04-12 17:12:47 +0200

    damn
    thats a lot of damage

---
+++ src/credentials.js
@@ -0,0 +1,5 @@
+exports.MEMCACHIER_USERNAME = "0929AC";
+exports.MEMCACHIER_PASSWORD = "74C9BE343BA76FCD9052B6A73836578E";
+exports.MEMCACHIER_SERVERS = "mc6.c1.eu-central-1.ec2.memcachier.com";
+exports.FACEBOOK_EMAIL = "smakubot@gmail.com";
+exports.FACEBOOK_PASSWORD = "uczucio1234567890";
